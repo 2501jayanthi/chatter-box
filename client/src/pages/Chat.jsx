61341021983cf99638332697e3f0c7f2ac50c2f3
2501jayanthi <2501jayanthi@gmail.com>
2024-07-11 09:56:13 +0530

    first  commit

---
+++ client/src/pages/Chat.jsx
@@ -0,0 +1,13 @@
+import { useContext } from "react";
+import { ChatContext } from "../context/ChatContext";
+
+const Chat = () => {
+    const { userChats,
+        isUserChatsLoading,
+        userChatsError} = useContext(ChatContext)
+
+
+    return <>Chat</>;
+}
+ 
+export default Chat ;
